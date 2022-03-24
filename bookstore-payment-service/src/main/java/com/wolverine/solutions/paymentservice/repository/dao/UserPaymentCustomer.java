6d7b971806c4a84c1a93c020e116e4f698dba8ba
Sumit Sarkar <sumit.sarkar@procor.com>
2022-03-24 17:49:09 -0400

    Make lowercase to all tables rows and columns of payment service

--- bookstore-payment-service/src/main/java/com/wolverine/solutions/paymentservice/repository/dao/UserPaymentCustomer.java
+++ bookstore-payment-service/src/main/java/com/wolverine/solutions/paymentservice/repository/dao/UserPaymentCustomer.java
@@ -19,13 +19,13 @@ import javax.persistence.Table;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "USER_PAYMENT_CUSTOMER")
+@Table(name = "user_payment_customer")
 @Builder
 public class UserPaymentCustomer extends DateAudit {
     @Id
     @GeneratedValue(generator = "uuid")
     @GenericGenerator(name = "uuid", strategy = "uuid2")
-    @Column(name = "ID", updatable = false, nullable = false)
+    @Column(name = "id", updatable = false, nullable = false)
     private String userPaymentCustomerId;
 
     @Column(name = "user_id", nullable = false, unique = true)
@@ -34,6 +34,6 @@ public class UserPaymentCustomer extends DateAudit {
     @Column(name = "user_name", nullable = false, unique = true)
     private String userName;
 
-    @Column(name = "PAYMENT_CUSTOMER_ID", nullable = false, unique = true)
+    @Column(name = "payment_customer_id", nullable = false, unique = true)
     private String paymentCustomerId;
 }
