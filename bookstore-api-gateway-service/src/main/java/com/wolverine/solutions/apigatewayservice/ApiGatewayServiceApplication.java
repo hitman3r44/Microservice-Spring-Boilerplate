a5a75ac997d5f0c8d423486b0fc9d413a488f868
Sumit Sarkar <sumit.sarkar@procor.com>
2022-04-29 01:05:37 -0400

    Reformat code

--- bookstore-api-gateway-service/src/main/java/com/wolverine/solutions/apigatewayservice/ApiGatewayServiceApplication.java
+++ bookstore-api-gateway-service/src/main/java/com/wolverine/solutions/apigatewayservice/ApiGatewayServiceApplication.java
@@ -5,14 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
 public class ApiGatewayServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayServiceApplication.class, args);
+    }
 }
