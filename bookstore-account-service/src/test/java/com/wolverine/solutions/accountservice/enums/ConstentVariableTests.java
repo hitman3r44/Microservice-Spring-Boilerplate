eab3bd40f0d3868cc6e2bf0d300d3762c93fe03f
Sumit Sarkar <sumit.sarkar@procor.com>
2022-07-28 07:32:47 -0400

    Change in to individual PORT into property file and add .env file into docker-compose.yml instead of individual entry

--- bookstore-account-service/src/test/java/com/wolverine/solutions/accountservice/enums/ConstentVariableTests.java
+++ bookstore-account-service/src/test/java/com/wolverine/solutions/accountservice/enums/ConstentVariableTests.java
@@ -16,8 +16,10 @@ public abstract class ConstentVariableTests {
     public static final String CONTROLLER_ROUTE = "business-profile/";
     public static final String jsonDumpFileName = "businessProfile";
     public static final String SERVER_NAME = "http://localhost:";
-    public static final String PORT = "8765";
-    public static final String URI = "/api/account/";
+    //    public static final String PORT = "8765";
+    public static final String PORT = "4001";
+    //    public static final String URI = "/api/account/";
+    public static final String URI = "/";
     public final static String PAGE_QUERY = "page-query?page=1&size=20&sort=id,asc";
     public static String lastID;
     public static String TOKEN = "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsid2ViIl0sInVzZXJfaWQiOiJ4Y3ZjdmJ2di1iYTVkLTRiOTItODViZS1kZmdkZmdkZmdkZmciLCJ1c2VyX25hbWUiOiJhZG1pbi5hZG1pbiIsInNjb3BlIjpbInJlYWQiLCJ3cml0ZSJdLCJleHAiOjE2NDg5NjU3NTgsImF1dGhvcml0aWVzIjpbIlNUQU5EQVJEX1VTRVIiLCJBRE1JTl9VU0VSIl0sImp0aSI6IkE1VENyNmhNNVRIaV9rRk1NMlIxUERYLWxWOCIsImNsaWVudF9pZCI6IjkzZWQ0NTNlLWI3YWMtNDE5Mi1hNmQ0LWM0NWZhZTBkOTlhYyJ9.D64UgnfVUzqUVPHQsC7ShmE05AFqfNSIX6ozzscqI-TjAmuyZtStRyReW4SgYW22P78JzvxbNLWpnrmBUuU-TcjnAxiYZBSWobDQL1mEPNH3_arXDqQo0CFmjHqaE333I-dKi6LZY4kiua_V-jmMjlMWbZeQYKGz9makRHdlAkjncLqfk6uEfuCrg-5RXkl9bWjfleBwgZHDmu1ggtKxoOf6sxNKMGAxb8giwNEuoxGwi8eGkx2SiteFCU2u5hqVDiXOfjOCCTnisAPkFe1jLhl0YaJdkchfCp4V1kvEKLYwT2lRyjgK3PYw7k6rybl4BIBYeeIrlB9YP-FsrLMuJw";
